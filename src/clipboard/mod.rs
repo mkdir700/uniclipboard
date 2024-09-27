5beb57e83db973df5720e341e78a618886a32664
mkdir700 <mkdir700@gmail.com>
2024-09-27 11:54:20 +0800

    refactor: 使用 clipboard-rs 重构 Localboard

--- src/clipboard/mod.rs
+++ src/clipboard/mod.rs
@@ -1,13 +1,3 @@
-#[cfg(any(target_os = "macos", target_os = "linux"))]
-pub mod arboard_clipboard;
-pub mod factory;
-pub mod traits;
-#[cfg(windows)]
-pub mod win_clipboard;
-
-#[cfg(any(target_os = "macos", target_os = "linux"))]
-pub use arboard_clipboard::ArboardClipboard;
-pub use factory::create_clipboard;
-pub use traits::ClipboardOperations;
-#[cfg(windows)]
-pub use win_clipboard::WinClipboard;
+pub mod clipboard;
+pub use clipboard::RsClipboard;
+pub use clipboard::RsClipboardChangeHandler;
