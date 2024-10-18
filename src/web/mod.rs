9d925daefea3f677449c619627c7e92f274e62bb
mkdir700 <mkdir700@gmail.com>
2024-10-18 09:47:44 +0800

    refactor: 处理 404 异常

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -4,6 +4,7 @@ mod routes;
 pub mod server;
 pub use handlers::websocket::WebSocketHandler;
 pub use handlers::websocket_message::WebSocketMessageHandler;
+use log::warn;
 pub use server::WebServer;
 
 use crate::errors::LockError;
@@ -13,13 +14,36 @@ use warp::Rejection;
 pub async fn handle_rejection(
     err: Rejection,
 ) -> Result<impl warp::Reply, std::convert::Infallible> {
+    if err.is_not_found() {
+        // 直接返回 404 错误，不使用 ApiResponse
+        return Ok(ApiResponse::<()>::error(
+            warp::http::StatusCode::NOT_FOUND.as_u16(),
+            "未找到资源".to_string(),
+        )
+        .into_response()
+        .unwrap());
+    }
+
     let (code, message) = if let Some(lock_error) = err.find::<LockError>() {
-        (500, lock_error.to_string())
+        (
+            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            lock_error.to_string(),
+        )
+    } else if let Some(warp_error) = err.find::<warp::Error>() {
+        (
+            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            warp_error.to_string(),
+        )
     } else {
-        (500, "内部服务器错误".to_string())
+        println!("Unhandled rejection: {:?}", err);
+        warn!("Unhandled rejection: {:?}", err);
+        (
+            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            "内部服务器错误".to_string(),
+        )
     };
 
-    Ok(ApiResponse::<()>::error(code, message)
+    Ok(ApiResponse::<()>::error(code.as_u16(), message)
         .into_response()
         .unwrap())
 }
