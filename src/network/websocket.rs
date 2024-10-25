715f7f9493fa03fc00bafa1a7cd1924a5ed405b0
mkdir700 <mkdir700@gmail.com>
2024-10-25 15:43:42 +0800

    refactor: 为 websocket client 新增成员变量 connected，用于标记当前连接的状态

--- src/network/websocket.rs
+++ src/network/websocket.rs
@@ -12,6 +12,8 @@ use futures_util::SinkExt;
 use log::error;
 use log::info;
 use log::warn;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use tokio::net::TcpStream;
 use tokio::sync::broadcast;
@@ -42,6 +44,7 @@ pub struct WebSocketClient {
     >,
     message_tx: Arc<broadcast::Sender<WebSocketMessage>>,
     message_join_handle: Option<tokio::task::JoinHandle<()>>,
+    connected: Arc<AtomicBool>,
 }
 
 impl WebSocketClient {
@@ -53,12 +56,15 @@ impl WebSocketClient {
             reader: Arc::new(None),
             message_tx: Arc::new(tx),
             message_join_handle: None,
+            connected: Arc::new(AtomicBool::new(false)),
         }
     }
 
-    pub async fn start_message_handler(&mut self) {
+    /// 开启一个异步任务，持续的从 reader 中接收消息并转发到 message_tx
+    async fn start_collect_messages(&mut self) {
         let reader = self.reader.clone();
         let message_tx = self.message_tx.clone();
+        let connected = self.connected.clone();
 
         self.message_join_handle = Some(tokio::spawn(async move {
             loop {
@@ -73,35 +79,50 @@ impl WebSocketClient {
                             }
                         }
                         Some(Err(e)) => {
-                            // TODO: 如果意外断开连接该如何处理？
-                            // 1. 尝试重连
-                            // 2. 发送消息通知上层
-                            error!("WebSocket error: {}", e);
+                            warn!("WebSocket error: {}", e);
                             break;
                         }
                         None => {
-                            error!("WebSocket connection closed");
+                            warn!("WebSocket connection closed");
                             break;
                         }
                     }
                 } else {
-                    error!("WebSocket reader not available");
+                    warn!("WebSocket reader not available");
                     break;
                 }
             }
+
+            info!("WebSocket message handler stopped");
+            // TODO!: 修改 connected 状态为 false
+            connected.store(false, Ordering::Relaxed);
         }));
     }
 
+    pub async fn stop_message_handler(&mut self) {
+        if let Some(handle) = self.message_join_handle.take() {
+            handle.abort();
+            self.message_join_handle = None;
+        }
+    }
+
+    /// 订阅消息, 返回一个 Receiver 对象
     pub fn subscribe(&self) -> broadcast::Receiver<WebSocketMessage> {
         self.message_tx.subscribe()
     }
 
+    /// 判断是否连接到服务器
+    pub fn is_connected(&self) -> bool {
+        self.connected.load(Ordering::Relaxed)
+    }
+
     /// 发送剪贴板同步消息
     /// 如果发送失败, 则尝试重新连接, 并重试3次
+    #[allow(dead_code)]
     pub async fn send_clipboard_sync(&mut self, message: &WebSocketMessage) -> Result<()> {
         let mut retries = 0;
         while retries < 3 {
-            if let Err(e) = self.send_raw(message).await {
+            if let Err(e) = self.send_with_websocket_message(message).await {
                 retries += 1;
                 error!("Failed to send message, retrying... {}", e);
                 // 捕获 IO error: Broken pipe(os error 32)
@@ -121,6 +142,7 @@ impl WebSocketClient {
     }
 
     /// 接收剪贴板同步消息
+    #[allow(dead_code)]
     pub async fn receive_clipboard_sync(&self) -> Result<Option<ClipboardSyncMessage>> {
         let message = self.subscribe().recv().await?;
         if let WebSocketMessage::ClipboardSync(data) = message {
@@ -129,7 +151,7 @@ impl WebSocketClient {
         Ok(None)
     }
 
-    pub async fn send_raw(&self, message: &WebSocketMessage) -> Result<()> {
+    pub async fn send_with_websocket_message(&self, message: &WebSocketMessage) -> Result<()> {
         let message = message.to_tungstenite_message();
         let mut writer = if let Some(writer) = self.writer.as_ref() {
             writer.lock().await
@@ -142,12 +164,26 @@ impl WebSocketClient {
         Ok(())
     }
 
+    pub async fn send_raw(&self, message: Message) -> Result<()> {
+        let mut writer = if let Some(writer) = self.writer.as_ref() {
+            writer.lock().await
+        } else {
+            return Err(anyhow::anyhow!(
+                "WebSocket error: Not connected, please connect first"
+            ));
+        };
+        writer.send(message).await?;
+        Ok(())
+    }
+
+    #[allow(dead_code)]
     pub async fn receive_raw(&self) -> Result<WebSocketMessage> {
         let message = self.subscribe().recv().await?;
         Ok(message)
     }
 
     /// 重新连接到服务器
+    #[allow(dead_code)]
     pub async fn reconnect(&mut self) -> Result<()> {
         self.disconnect().await?;
         self.connect().await
@@ -175,8 +211,8 @@ impl WebSocketClient {
             self.reader = Arc::new(Some(Mutex::new(reader)));
         }
 
-        self.start_message_handler().await;
-
+        self.start_collect_messages().await;
+        self.connected.store(true, Ordering::Relaxed);
         Ok(())
     }
 
@@ -199,10 +235,8 @@ impl WebSocketClient {
         // 现在可以安全地修改 self.writer 和 self.reader
         self.writer = Arc::new(None);
         self.reader = Arc::new(None);
-        if let Some(handle) = self.message_join_handle.take() {
-            handle.abort();
-        }
-        self.message_join_handle = None;
+        self.stop_message_handler().await;
+        self.connected.store(false, Ordering::Relaxed);
         Ok(())
     }
 
@@ -218,7 +252,7 @@ impl WebSocketClient {
         });
 
         let web_socket_message = WebSocketMessage::Register(RegisterDeviceMessage::new(
-            device.id,
+            device.id.clone(),
             device.ip,
             device.server_port,
         ));
@@ -231,45 +265,28 @@ impl WebSocketClient {
             ));
         };
         writer.send(Message::Text(message)).await?;
+        info!("Register [{}] to server({})", device.id, self.uri);
         Ok(())
     }
 
     /// 向其他设备同步当前设备已知的设备列表
     pub async fn sync_device_list(&self) -> Result<()> {
         let device_id = CONFIG.read().unwrap().device_id.clone();
-        let devices = get_device_manager().get_all_devices().map_err(|_| {
-            anyhow::anyhow!("Failed to get all devices")
-        })?;
+        let devices = get_device_manager()
+            .get_all_devices()
+            .map_err(|_| anyhow::anyhow!("Failed to get all devices"))?;
 
         let web_socket_message = WebSocketMessage::DeviceListSync(DevicesSyncMessage {
             devices: devices.iter().map(|d| DeviceSyncInfo::from(d)).collect(),
             replay_device_ids: vec![device_id],
         });
-        self.send_raw(&web_socket_message).await?;
+        self.send_with_websocket_message(&web_socket_message)
+            .await?;
         Ok(())
     }
 
-    // pub async fn start_ping_task(&self, interval_secs: u64) {
-    //     let mut ping_interval = interval(Duration::from_secs(interval_secs));
-    //     let writer = self.writer.clone();
-
-    //     tokio::spawn(async move {
-    //         loop {
-    //             tokio::select! {
-    //                 _ = ping_interval.tick() => {
-    //                     if let Some(writer) = writer.as_ref() {
-    //                         let mut writer = writer.lock().await;
-    //                         if let Err(e) = writer.send(Message::Ping(vec![])).await {
-    //                             error!("发送 ping 失败: {}", e);
-    //                             break;
-    //                         }
-    //                     } else {
-    //                         error!("WebSocket 写入器不可用");
-    //                         break;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     });
-    // }
+    async fn ping(&self) -> Result<()> {
+        self.send_raw(Message::Ping(vec![])).await?;
+        Ok(())
+    }
 }
