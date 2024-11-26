52643fa796120cf218238cdf425c09459c36072e
mkdir700 <mkdir700@gmail.com>
2024-11-26 11:26:39 +0800

    fix(ci): 修复发布流程和 artifacts 处理

--- scripts/build-cross-release.sh
+++ scripts/build-cross-release.sh
@@ -14,18 +14,22 @@ echo "Starting build-cross-release.sh"
 # 将提前编译好的二进制文件复制到dist目录下，并重命名为统一的名称，方便后续上传到release中。
 [ -f artifacts/x86_64-pc-windows-msvc-uniclipboard/uniclipboard.exe ] &&
   mkdir -p dist/uniclipboard_windows_amd64_v1/ &&
+  rm -rf dist/uniclipboard_windows_amd64_v1/uniclipboard.exe &&
   cp artifacts/x86_64-pc-windows-msvc-uniclipboard/uniclipboard.exe dist/uniclipboard_windows_amd64_v1/uniclipboard.exe
 
 [ -f artifacts/x86_64-unknown-linux-gnu-uniclipboard/uniclipboard ] &&
   mkdir -p dist/uniclipboard_linux_amd64_v1/ &&
+  rm -rf dist/uniclipboard_linux_amd64_v1/uniclipboard &&
   cp artifacts/x86_64-unknown-linux-gnu-uniclipboard/uniclipboard dist/uniclipboard_linux_amd64_v1/uniclipboard
 
 [ -f artifacts/x86_64-apple-darwin-uniclipboard/uniclipboard ] &&
   mkdir -p dist/uniclipboard_darwin_amd64_v1/ &&
+  rm -rf dist/uniclipboard_darwin_amd64_v1/uniclipboard &&
   cp artifacts/x86_64-apple-darwin-uniclipboard/uniclipboard dist/uniclipboard_darwin_amd64_v1/uniclipboard
 
 [ -f artifacts/aarch64-apple-darwin-uniclipboard/uniclipboard ] &&
   mkdir -p dist/uniclipboard_darwin_arm64/ &&
+  rm -rf dist/uniclipboard_darwin_arm64/uniclipboard &&
   cp artifacts/aarch64-apple-darwin-uniclipboard/uniclipboard dist/uniclipboard_darwin_arm64/uniclipboard
 
 echo "Finished build-cross-release.sh"
